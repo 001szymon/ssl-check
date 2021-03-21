4bdab6ef9748e858cca2dff34aea41af745c021b
Szymon <001szymon@gmail.com>
2021-03-21 16:57:49 +0100

    Update sslcheck.sh

--- sslcheck.sh
+++ sslcheck.sh
@@ -206,9 +206,8 @@ check_file_status() {
 
     ### Grab the common name (CN) from the X.509 certificate
     COMMONNAME=$("${OPENSSL}" x509 -in "${CERTFILE}" -subject -noout -inform pem | \
-                    "${AWK}" 'BEGIN{FS="Subject: "} NF==2{print $2}')
-                    # "${SED}" -e 's/.*CN = //' | \
-                    # "${SED}" -e 's/, .*//')
+                    "${SED}" -e 's/.*CN = //' | \
+                    "${SED}" -e 's/, .*//')
                     #"${SED}" 's/^subject= //')
                     #"${AWK}" -F'=' '/CN=/ { print $2 }')
 
